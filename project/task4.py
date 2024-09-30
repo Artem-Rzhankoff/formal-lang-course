4d2133c8e5de4f341d953102261427952a89320c
Artem Rhzankov <Artemr2004@rambler.ru>
2024-09-30 23:02:00 +0300

    refactor: apply formatters

--- project/task4.py
+++ project/task4.py
@@ -5,30 +5,37 @@ from scipy.sparse import bsr_matrix, vstack
 from project.task2 import regex_to_dfa, graph_to_nfa
 from project.task3 import AdjacencyMatrixFA
 
-def ms_bfs_based_rpq(regex: str, graph: MultiDiGraph, start_nodes: set[int],
-         final_nodes: set[int]) -> set[tuple[int, int]]:
-    
+
+def ms_bfs_based_rpq(
+    regex: str, graph: MultiDiGraph, start_nodes: set[int], final_nodes: set[int]
+) -> set[tuple[int, int]]:
     adj_matrix_dfa = AdjacencyMatrixFA(regex_to_dfa(regex))
     adj_matrix_nfa = AdjacencyMatrixFA(graph_to_nfa(graph, start_nodes, final_nodes))
 
-    transposed_matricies : dict[Symbol, bsr_matrix] = {}
+    transposed_matricies: dict[Symbol, bsr_matrix] = {}
     for symbol, matrix in adj_matrix_dfa.matricies.items():
         transposed_matricies[symbol] = matrix.transpose()
-        
+
     dfa_states_count = adj_matrix_dfa.states_count
     dfa_start_state = list(adj_matrix_dfa.start_states)[0]
 
     nfa_states_count = adj_matrix_nfa.states_count
     nfa_start_states = adj_matrix_nfa.start_states
 
-    front = vstack([
-        bsr_matrix(([True], ([dfa_start_state], [nfa_start_state])), shape=(dfa_states_count, nfa_states_count), dtype=bool)
-        for nfa_start_state in nfa_start_states
-        ])
+    front = vstack(
+        [
+            bsr_matrix(
+                ([True], ([dfa_start_state], [nfa_start_state])),
+                shape=(dfa_states_count, nfa_states_count),
+                dtype=bool,
+            )
+            for nfa_start_state in nfa_start_states
+        ]
+    )
     visited = front
 
     symbols = adj_matrix_dfa.matricies.keys() & adj_matrix_nfa.matricies.keys()
-    
+
     while front.count_nonzero() > 0:
         next_fronts: dict[Symbol, bsr_matrix] = {}
         for s in symbols:
@@ -36,7 +43,7 @@ def ms_bfs_based_rpq(regex: str, graph: MultiDiGraph, start_nodes: set[int],
             next_fronts[s] = vstack(
                 [
                     transposed_matricies[s]
-                    @ next_front[dfa_states_count * i : dfa_states_count * (i+1)]
+                    @ next_front[dfa_states_count * i : dfa_states_count * (i + 1)]
                     for i in range(len(start_nodes))
                 ]
             )
@@ -44,18 +51,20 @@ def ms_bfs_based_rpq(regex: str, graph: MultiDiGraph, start_nodes: set[int],
         front = front > visited
         visited += front
 
-    result: set[tuple[int, int]] = set()
+    result = set()
     reversed_nfa_states = {value: key for key, value in adj_matrix_nfa.states.items()}
 
-    for dfa_final in adj_matrix_dfa.final_states:
-        for i, nfa_start in enumerate(nfa_start_states): 
-
-            visited_slice = visited[
-                dfa_states_count * i : dfa_states_count * (i + 1)
-            ]
-
-            for nfa_reached in visited_slice.getrow(dfa_final).indices:
+    for dfa_fn_state in adj_matrix_dfa.final_states:
+        for i, nfa_start_state in enumerate(nfa_start_states):
+            for nfa_reached in visited.getrow(
+                dfa_states_count * i + dfa_fn_state
+            ).indices:
                 if nfa_reached in adj_matrix_nfa.final_states:
-                    result.add((reversed_nfa_states[nfa_start], reversed_nfa_states[nfa_reached]))
+                    result.add(
+                        (
+                            reversed_nfa_states[nfa_start_state],
+                            reversed_nfa_states[nfa_reached],
+                        )
+                    )
 
     return result
