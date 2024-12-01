68e37ee75011e303c488440f79e7e271c8d8df24
Artem Rhzankov <Artemr2004@rambler.ru>
2024-12-01 17:01:48 +0300

    feat: functions

--- project/task11.py
+++ project/task11.py
@@ -1,39 +1,43 @@
-from antlr4 import ParserRuleContext, CommonTokenStream, InputStream
+from antlr4 import ParserRuleContext, CommonTokenStream, InputStream, ParseTreeWalker, TerminalNode
 from project.GraphLanguageListener import GraphLanguageListener
 from project.GraphLanguageLexer import GraphLanguageLexer
 from project.GraphLanguageParser import GraphLanguageParser
 
 
 def _parse(program: str) -> GraphLanguageParser:
-    return GraphLanguageParser(CommonTokenStream(GraphLanguageLexer(InputStream(program))) )
+    return GraphLanguageParser(CommonTokenStream(GraphLanguageLexer(InputStream(program))))
 
 # Второе поле показывает корректна ли строка (True, если корректна)
 def program_to_tree(program: str) -> tuple[ParserRuleContext, bool]:
     parser = _parse(program)
     parser.removeErrorListeners()
-    parser.program()
 
-    is_correct = parser.getNumberOfSyntaxErrors() == 0
     tree = parser.program()
-    print(tree.toStringTree())
+    is_correct = parser.getNumberOfSyntaxErrors() == 0
     return (tree, is_correct)
 
-
-
 def nodes_count(tree: ParserRuleContext) -> int:
-    pass
+    listener = TreeListener()
+    walker = ParseTreeWalker()
+
+    walker.walk(listener, tree)
+
+    return listener.nodes_count
 
 def tree_to_program(tree: ParserRuleContext) -> str:
-    pass
+    if isinstance(tree, TerminalNode):
+        return tree.getText()
+    result = []
+    for child in tree.children:
+        child_text = tree_to_program(child)
+        result.append(' ')
+        result.append(child_text)
+    
+    return ''.join(result)
 
 class TreeListener(GraphLanguageListener):
-
     def __init__(self):
         self.nodes_count = 0
-        self.stack = []
 
     def enterEveryRule(self, ctx):
-        self.nodes_count += 1
-
-    def visitTerminal(self, node):
         self.nodes_count += 1
